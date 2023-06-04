2b538b56f9cfd4cd116f15c7c05757a4825e8aee
bam-e22 <stack.07142@gmail.com>
2023-06-04 23:51:11 +0900

    adjustResize

--- app/src/main/java/com/example/tiktok/MainActivity.kt
+++ app/src/main/java/com/example/tiktok/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.tiktok
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.ui.Modifier
 import com.example.tiktok.navigation.TikTokNavGraph
 import com.example.tiktok.ui.theme.TiktokTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TiktokTheme {
-                TikTokNavGraph()
+                TikTokNavGraph(
+                    modifier = Modifier
+                        .statusBarsPadding()
+                        .navigationBarsPadding()
+                        .imePadding()
+                )
             }
         }
     }
--- app/src/main/java/com/example/tiktok/navigation/TikTokNavGraph.kt
+++ app/src/main/java/com/example/tiktok/navigation/TikTokNavGraph.kt
@@ -1,16 +1,19 @@
 package com.example.tiktok.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun TikTokNavGraph(
+    modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
     startDestination: String = AuthGraphRoute, // TODO: 메인 루트로 변경
 ) {
     NavHost(
+        modifier = modifier,
         navController = navController,
         startDestination = startDestination
     ) {
