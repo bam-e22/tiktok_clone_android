9c6d39ce160313750c360827ab03d5fe2e9eab08
bam-e22 <stack.07142@gmail.com>
2023-07-12 15:49:28 +0900

    detekt 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -6,4 +8,21 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.detekt)
+}
+
+val projectSource = file(projectDir)
+val configFile = files("$rootDir/config/detekt/detekt.yml")
+val kotlinFiles = "**/*.kt"
+val resourceFiles = "**/resources/**"
+val buildFiles = "**/build/**"
+tasks.register<Detekt>("detektAll") {
+    description = "Runs a custom detekt build."
+
+    ignoreFailures = true
+    buildUponDefaultConfig = true
+    setSource(projectSource)
+    config.setFrom(configFile)
+    include(kotlinFiles)
+    exclude(resourceFiles, buildFiles)
 }
