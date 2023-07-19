038ab3ad5139b51d49d05cfaa65d259971bae3e3
bam-e22 <stack.07142@gmail.com>
2023-07-19 20:07:37 +0900

    add VideoComments BottomSheet

---
+++ app/src/main/java/com/example/tiktok/ui/components/SnsButton.kt
@@ -0,0 +1,48 @@
+package com.example.tiktok.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.tiktok.R
+import com.example.tiktok.ui.utils.Sizes
+
+@Composable
+fun SnsButton(
+    imageVector: ImageVector,
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        IconButton(
+            onClick = onClick
+        ) {
+            Icon(
+                modifier = Modifier.size(Sizes.LargeIconSize),
+                imageVector = imageVector,
+                tint = Color.White,
+                contentDescription = stringResource(id = R.string.cd_favorite)
+            )
+        }
+        Spacer(modifier = Modifier.height(1.dp))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelLarge.copy(color = Color.White),
+        )
+    }
+}
--- app/src/main/java/com/example/tiktok/ui/timeline/VideoPost.kt
+++ app/src/main/java/com/example/tiktok/ui/timeline/VideoPost.kt
@@ -2,37 +2,73 @@ package com.example.tiktok.ui.timeline
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.outlined.ChatBubbleOutline
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.tiktok.R
 import com.example.tiktok.model.VideoItem
 import com.example.tiktok.ui.components.CircleAvatar
+import com.example.tiktok.ui.components.SnsButton
 import com.example.tiktok.ui.components.VideoPlayer
 import com.example.tiktok.ui.utils.Sizes
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VideoPost(
     videoItem: VideoItem,
     modifier: Modifier = Modifier
 ) {
+    var showSheet by remember { mutableStateOf(false) }
+    val modalBottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    if (showSheet) {
+        ModalBottomSheet(
+            modifier = Modifier.fillMaxHeight(fraction = 0.7f),
+            sheetState = modalBottomSheetState,
+            scrimColor = Color.Transparent,
+            dragHandle = null,
+            onDismissRequest = {
+                showSheet = false
+            }
+        ) {
+            VideoComments(
+                comments = comments,
+                onClose = {
+                    showSheet = false
+                }
+            )
+        }
+    }
+
     Box(
         modifier = modifier
     ) {
@@ -46,7 +82,10 @@ fun VideoPost(
         VideoSnsButtons(
             modifier = Modifier
                 .align(Alignment.BottomEnd)
-                .padding(end = 20.dp, bottom = 20.dp)
+                .padding(end = 20.dp, bottom = 20.dp),
+            onCommentTap = {
+                showSheet = true
+            }
         )
     }
 }
@@ -92,6 +131,7 @@ private fun VideoInfo(
 @Composable
 private fun VideoSnsButtons(
     modifier: Modifier = Modifier,
+    onCommentTap: () -> Unit
 ) {
     Column(
         modifier = modifier,
@@ -111,7 +151,7 @@ private fun VideoSnsButtons(
         SnsButton(
             imageVector = Icons.Outlined.ChatBubbleOutline,
             text = "0",
-            onClick = {}
+            onClick = onCommentTap
         )
         Spacer(modifier = Modifier.height(20.dp))
         SnsButton(
@@ -123,31 +163,64 @@ private fun VideoSnsButtons(
     }
 }
 
+val comments = listOf(
+    Pair("United States", "\uD83C\uDDFA\uD83C\uDDF8"),
+    Pair("Canada", "\uD83C\uDDE8\uD83C\uDDE6"),
+    Pair("India", "\uD83C\uDDEE\uD83C\uDDF3"),
+    Pair("Germany", "\uD83C\uDDE9\uD83C\uDDEA"),
+    Pair("France", "\uD83C\uDDEB\uD83C\uDDF7"),
+    Pair("Japan", "\uD83C\uDDEF\uD83C\uDDF5"),
+    Pair("China", "\uD83C\uDDE8\uD83C\uDDF3"),
+    Pair("Brazil", "\uD83C\uDDE7\uD83C\uDDF7"),
+    Pair("Australia", "\uD83C\uDDE6\uD83C\uDDFA"),
+    Pair("Russia", "\uD83C\uDDF7\uD83C\uDDFA"),
+    Pair("United Kingdom", "\uD83C\uDDEC\uD83C\uDDE7"),
+)
+
 @Composable
-private fun SnsButton(
-    imageVector: ImageVector,
-    text: String,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
+private fun VideoComments(
+    comments: List<Pair<String, String>>,
+    modifier: Modifier = Modifier,
+    onClose: () -> Unit
 ) {
     Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier.fillMaxSize()
     ) {
-        IconButton(
-            onClick = onClick
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
         ) {
-            Icon(
-                modifier = Modifier.size(Sizes.LargeIconSize),
-                imageVector = imageVector,
-                tint = Color.White,
-                contentDescription = stringResource(id = R.string.cd_favorite)
+            Text(
+                "댓글 ${comments.size}개",
+                modifier = Modifier.align(Alignment.Center),
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold)
             )
+            IconButton(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                onClick = onClose
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    modifier = Modifier.size(Sizes.extraSmallIconSize),
+                    contentDescription = stringResource(id = R.string.cd_close)
+                )
+            }
         }
-        Spacer(modifier = Modifier.height(1.dp))
-        Text(
-            text = text,
-            style = MaterialTheme.typography.labelLarge.copy(color = Color.White),
-        )
+        LazyColumn {
+            items(comments) { (country, flag) ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp, horizontal = 20.dp)
+                ) {
+                    Text(
+                        text = flag,
+                        modifier = Modifier.padding(end = 20.dp)
+                    )
+                    Text(text = country)
+                }
+            }
+        }
     }
 }
